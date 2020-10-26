089f0115884931168feb6dc8c41d8dabf0f2ece7
Mark <cusx1981@gmail.com>
2020-10-26 17:26:37 +0000

    commit

--- php/getWeatherInfo.php
+++ php/getWeatherInfo.php
@@ -1,11 +1,11 @@
 <?php
-
-$url = 'api.geonames.org/countrySubdivisionJSON?lat=' . $_REQUEST['lat']  . '&lng=' .  $_REQUEST['lon']  . '&username=cusx1981';
-
-
 $executionStartTime = microtime(true) / 1000;
-$ch = curl_init();
 
+
+$url = 'http://api.geonames.org/findNearByWeatherJSON?lat=' . $_REQUEST['lat'] . '&lng=' . $_REQUEST['lon'] . '&username=cusx1981';
+
+
+$ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL,$url);
@@ -24,11 +24,10 @@ $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
 $output['status']['description'] = "mission saved";
 $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-$output['data'] = $decode['countryCode'];
+$output['data'] = $decode['weatherObservation'];
 	
 header('Content-Type: application/json; charset=UTF-8');
 
 echo json_encode($output); 
-
 
 ?>
